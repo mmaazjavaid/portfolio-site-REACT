a553de03e825fcf0e04e0fdeece2e204a2ef86a0
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-06-15 10:53:28 +0500

    sidebar complpeted

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,6 +5,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedinIn,faGithub,faInstagram ,faFacebook } from '@fortawesome/free-brands-svg-icons'
 function Sidebar() {
   return (
     <div className='nav-bar'>
@@ -23,6 +24,45 @@ function Sidebar() {
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/muhammad-maaz-a456ba1b8/"
+                >
+                    <FontAwesomeIcon icon={faLinkedinIn}  color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/mmaazjavaid"
+                >
+                    <FontAwesomeIcon icon={faGithub}  color="#4d4d4e" />
+                </a>
+            </li>
+
+            <li>
+                <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.instagram.com/m_maaz_javaid/"
+                >
+                    <FontAwesomeIcon icon={faInstagram}  color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.facebook.com/profile.php?id=100004667566440"
+                >
+                    <FontAwesomeIcon icon={faFacebook}  color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
   )
 }
